dda4455f56266cb706598941b56aaf97303da519
itsmht <iamtalha.mht@gmail.com>
2024-12-23 15:41:24 +0600

    small update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,7 +12,7 @@
                     <div class="row">
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="page-header">
-                                <h2 class="pageheader-title">Quirky Smart Control Panel </h2>
+                                <h2 class="pageheader-title">Smart Control Panel </h2>
                                 
                             </div>
                         </div>
